59f3e9f167c23d26099ae4d4013851f0d30d24fd
FieryRaccoon23 <araib.saleem@gmail.com>
2018-05-03 18:39:41 -0400

    Channel Separation functions

--- ImageMatrixTools.h
+++ ImageMatrixTools.h
@@ -11,6 +11,12 @@ public:
 	int ImageToMatrix(Eigen::MatrixXd*& eigenMatrix, std::string location);
 
 	static
+	int RGBImageToRGBMatrix(Eigen::MatrixXd*& red, Eigen::MatrixXd*& green, Eigen::MatrixXd*& blue, std::string location);
+
+	static
 	void MatrixToImage(Eigen::MatrixXd* eigenMatrix, std::string location);
+
+	static
+	void RGBMatrixToRGBImage(Eigen::MatrixXd* red, Eigen::MatrixXd* green, Eigen::MatrixXd* blue, std::string location);
 };
 
