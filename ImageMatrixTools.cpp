151bdb07d41287b1408d55357a1e8beeeb330c88
FieryRaccoon23 <araib.saleem@gmail.com>
2018-05-03 18:38:52 -0400

    Channel Seperation + heap memory

--- ImageMatrixTools.cpp
+++ ImageMatrixTools.cpp
@@ -21,8 +21,6 @@ int ImageMatrixTools::ImageToMatrix(Eigen::MatrixXd*& eigenMatrix, std::string l
 		return -1;
 	}
 
-	//Eigen::Map<Eigen::Matrix<float, Eigen::Dynamic, Eigen::Dynamic, Eigen::RowMajor>> imageEigen(image.ptr<float>(), image.rows, image.cols);
-
 	//cv::namedWindow("Display window", cv::WINDOW_AUTOSIZE);// Create a window for display.
 	//imshow("Display window", image);                   // Show our image inside it.
 	//
@@ -30,8 +28,11 @@ int ImageMatrixTools::ImageToMatrix(Eigen::MatrixXd*& eigenMatrix, std::string l
 
 	int imageRows = image.rows;
 	int imageCols = image.cols;
-	int imageChannels = image.channels();
+	//int imageChannels = image.channels();
 	int offset = 0;
+
+	eigenMatrix = new Eigen::MatrixXd(imageRows, imageCols * 3);
+
 	for (int i = 0; i < imageRows; ++i)
 	{
 		for (int j = 0; j < imageCols; ++j)
@@ -50,9 +51,42 @@ int ImageMatrixTools::ImageToMatrix(Eigen::MatrixXd*& eigenMatrix, std::string l
 	return 0;
 }
 
+int ImageMatrixTools::RGBImageToRGBMatrix(Eigen::MatrixXd*& red, Eigen::MatrixXd*& green, Eigen::MatrixXd*& blue, std::string location)
+{
+	cv::Mat image;
+	image = cv::imread(location);
+
+	if (!image.data)
+	{
+		std::cout << "Could not open or find the image" << std::endl;
+		return -1;
+	}
+
+	int imageRows = image.rows;
+	int imageCols = image.cols;
+
+	red   = new Eigen::MatrixXd(imageRows, imageCols);
+	green = new Eigen::MatrixXd(imageRows, imageCols);
+	blue  = new Eigen::MatrixXd(imageRows, imageCols);
+
+	for (int i = 0; i < imageRows; ++i)
+	{
+		for (int j = 0; j < imageCols; ++j)
+		{
+			cv::Vec3b bgrPixel = image.at<cv::Vec3b>(i, j);
+			red->data()[i*imageCols + j] = bgrPixel[2];
+			green->data()[i*imageCols + j] = bgrPixel[1];
+			blue->data()[i*imageCols + j] = bgrPixel[0];
+		}
+	}
+
+	return 0;
+}
 
 void ImageMatrixTools::MatrixToImage(Eigen::MatrixXd* eigenMatrix, std::string location)
 {
+	assert(eigenMatrix != nullptr);
+
 	int imageRows = eigenMatrix->rows();
 	int imageCols = eigenMatrix->cols() / 3;
 
@@ -70,6 +104,32 @@ void ImageMatrixTools::MatrixToImage(Eigen::MatrixXd* eigenMatrix, std::string l
 			image.at<cv::Vec3b>(i, j) = bgrPixel;
 
 			offset += 3;
+		}
+	}
+
+	cv::imwrite(location, image);
+}
+
+void ImageMatrixTools::RGBMatrixToRGBImage(Eigen::MatrixXd* red, Eigen::MatrixXd* green, Eigen::MatrixXd* blue, std::string location)
+{
+	assert(red != nullptr);
+	assert(green != nullptr);
+	assert(blue != nullptr);
+
+	int imageRows = red->rows();
+	int imageCols = red->cols();
+
+	cv::Mat image(imageRows, imageCols, CV_8UC3);
+
+	for (int i = 0; i < imageRows; ++i)
+	{
+		for (int j = 0; j < imageCols; ++j)
+		{
+			cv::Vec3b bgrPixel;
+			bgrPixel[2] = red->data()[j];
+			bgrPixel[1] = green->data()[j];
+			bgrPixel[0] = blue->data()[j];
+			image.at<cv::Vec3b>(i, j) = bgrPixel;
 		}
 	}
 
