31aa48e90f82d5845b76809b4049bf6678f0c7f5
FieryRaccoon23 <araib.saleem@gmail.com>
2018-06-07 14:45:33 -0400

    First implementation of Inpainting algorithm
    - Few of the parameters might not be required. The final version will have a cleaner function signature

--- Solver.h
+++ Solver.h
@@ -1,8 +1,14 @@
+#include "IncludedLibraries.h"
+
 #pragma once
 class Solver
 {
 public:
 	Solver();
 	~Solver();
+
+    void InPaintingSolver(Eigen::MatrixXd*& CompletedMatrix, Eigen::MatrixXd* InputMatrix, Eigen::MatrixXd* MaskMatrix, float tol = 10e-7,
+                          float softThreshold = 5.f, float step = 0.1f, float increment = 1.f, float parameter = 900.f, float l2RelaxationParamter = 2.f,
+                          int iterations = 100);
 };
 
